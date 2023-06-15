506e8db603fe078b7324bb5736e6bee6631712e9
MATEUS KOVALSKI <50926988+mkmoura@users.noreply.github.com>
2023-06-15 13:12:08 -0300

    Update content index.php

--- index.php
+++ index.php
@@ -26,65 +26,32 @@
       Site em construção. Volte em breve!
     </p> 
     
-    <div class="container site-content">
-                            <div class="row">
-                                <main id="main" class="site-main col-sm-12 full-width">
-                                    <article id="post-139" class="post-139 page type-page status-publish hentry">
-                                        <div class="entry-content">
-                                            <div class="vc_row wpb_row vc_row-fluid">
-                                                <div class="left-column">
-                                                    <div class="vc_column-inner">
-                                                        <div class="wpb_wrapper">
-                                                            <div class="wpb_single_image wpb_content_element vc_align_left  wpb_animate_when_almost_visible wpb_left-to-right left-to-right">
-                                                                <figure class="wpb_wrapper vc_figure">
-                                                                    <a href="https://umbrasil.org.br/wp-content/uploads/2010/01/MAPA-BRASIL-MARISTA-1.jpg" target="_self" class="vc_single_image-wrapper   vc_box_border_grey">
-                                                                        <img width="600" height="531" src="https://umbrasil.org.br/wp-content/uploads/2010/01/MAPA-BRASIL-MARISTA-1.jpg" class="vc_single_image-img attachment-full" alt="" loading="lazy" srcset="https://umbrasil.org.br/wp-content/uploads/2010/01/MAPA-BRASIL-MARISTA-1.jpg 600w, https://umbrasil.org.br/wp-content/uploads/2010/01/MAPA-BRASIL-MARISTA-1-300x266.jpg 300w" sizes="(max-width: 600px) 100vw, 600px"/>
-                                                                    </a>
-                                                                </figure>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="right-column">
-                                                    <div class="vc_column-inner">
-                                                        <div class="wpb_wrapper">
-                                                            <div class="wpb_text_column wpb_content_element ">
-                                                                <div class="wpb_wrapper">
-                                                                    <div class="vc_row wpb_row vc_row-fluid"></div>
-                                                                    <div class="wpb_column vc_column_container vc_col-sm-12">
-                                                                        <div class="vc_column-inner">
-                                                                            <div class="wpb_wrapper"></div>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="wpb_text_column wpb_content_element ">
-                                                                        <div class="wpb_wrapper"></div>
-                                                                    </div>
-                                                                    <p>Presente em mais de 80 países, a ação marista mantém escolas, universidades, unidades sociais, centros de evangelização, editoras, veículos de comunicação e hospitais. São aproximadamente 3.500 Irmãos que partilham suas tarefas de maneira direta com 72 mil Leigos, promovendo a missão nos cinco continentes, beneficiando mais de 654 mil crianças.</p>
-                                                                    <p>No Brasil, os primeiros Irmãos Maristas chegaram em 1897 dispostos a dar continuidade ao projeto de formar bons cristãos e virtuosos cidadãos. O Brasil Marista está presente em 23 estados e no Distrito Federal. São 98 cidades brasileiras, mais de 27 mil Irmãos, Leigas, Leigos e colaboradores, mais de 80 mil alunos distribuídos em unidades de educação básica, mais de 58 mil alunos em unidades de ensino superior, mais de 2 milhões atendimentos somente pelo Sistema Único de Saúde (SUS) e mais de 30 mil pessoas beneficiadas na solidariedade.</p>
-                                                                    <p>O Brasil Marista divide-se em três unidades administrativas: Província Marista Brasil Centro-Norte, Província Marista Brasil Sul-Amazônia (Rede Marista) e Província Marista Brasil Centro-Sul (Grupo Marista). O apoio administrativo se dá por meio de mantenedoras, estruturas responsáveis pelo gerenciamento nas diversas frentes de atuação. A União Marista do Brasil (UMBRASIL) é a associação das mantenedoras, representando o universo Marista no país.</p>
-                                                                    <p>As 10 associadas têm, atualmente, cerca de 30 mil colaboradores.</p>
-                                                                    <p style="text-align: center;">
-                                                                        <a href="http://marista.edu.br/" target="_blank" rel="noopener noreferrer">Província Marista Brasil Centro-Norte</a>
-                                                                    </p>
-                                                                    <p style="text-align: center;">
-                                                                        <a href="https://marista.org.br/" target="_blank" rel="noopener noreferrer">Província Marista Brasil Centro-Sul</a>
-                                                                    </p>
-                                                                    <p style="text-align: center;">
-                                                                        <a href="http://maristas.org.br/" target="_blank" rel="noopener noreferrer">Província Marista Brasil Sul-Amazônia</a>
-                                                                    </p>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!-- .entry-content -->
-                                    </article>
-                                    <!-- #post-## -->
-                                </main>
-                            </div>
-                        </div>
+    <div style="display: flex;">
+    <div style="flex: 1;">
+
+
+      <a href="https://umbrasil.org.br/wp-content/uploads/2010/01/MAPA-BRASIL-MARISTA-1.jpg" target="_self" class="vc_single_image-wrapper   vc_box_border_grey">
+          <img width="600" height="531" src="https://umbrasil.org.br/wp-content/uploads/2010/01/MAPA-BRASIL-MARISTA-1.jpg" class="vc_single_image-img attachment-full" alt="" loading="lazy" srcset="https://umbrasil.org.br/wp-content/uploads/2010/01/MAPA-BRASIL-MARISTA-1.jpg 600w, https://umbrasil.org.br/wp-content/uploads/2010/01/MAPA-BRASIL-MARISTA-1-300x266.jpg 300w" sizes="(max-width: 600px) 100vw, 600px"/>
+      </a>
+    </div>
+      
+      
+  <div style="flex: 1;">
+
+                      <p>Presente em mais de 80 países, a ação marista mantém escolas, universidades, unidades sociais, centros de evangelização, editoras, veículos de comunicação e hospitais. São aproximadamente 3.500 Irmãos que partilham suas tarefas de maneira direta com 72 mil Leigos, promovendo a missão nos cinco continentes, beneficiando mais de 654 mil crianças.</p>
+                      <p>No Brasil, os primeiros Irmãos Maristas chegaram em 1897 dispostos a dar continuidade ao projeto de formar bons cristãos e virtuosos cidadãos. O Brasil Marista está presente em 23 estados e no Distrito Federal. São 98 cidades brasileiras, mais de 27 mil Irmãos, Leigas, Leigos e colaboradores, mais de 80 mil alunos distribuídos em unidades de educação básica, mais de 58 mil alunos em unidades de ensino superior, mais de 2 milhões atendimentos somente pelo Sistema Único de Saúde (SUS) e mais de 30 mil pessoas beneficiadas na solidariedade.</p>
+                      <p>O Brasil Marista divide-se em três unidades administrativas: Província Marista Brasil Centro-Norte, Província Marista Brasil Sul-Amazônia (Rede Marista) e Província Marista Brasil Centro-Sul (Grupo Marista). O apoio administrativo se dá por meio de mantenedoras, estruturas responsáveis pelo gerenciamento nas diversas frentes de atuação. A União Marista do Brasil (UMBRASIL) é a associação das mantenedoras, representando o universo Marista no país.</p>
+                      <p>As 10 associadas têm, atualmente, cerca de 30 mil colaboradores.</p>
+                      <p style="text-align: center;">
+                          <a href="http://marista.edu.br/" target="_blank" rel="noopener noreferrer">Província Marista Brasil Centro-Norte</a>
+                      </p>
+                      <p style="text-align: center;">
+                          <a href="https://marista.org.br/" target="_blank" rel="noopener noreferrer">Província Marista Brasil Centro-Sul</a>
+                      </p>
+                      <p style="text-align: center;">
+                          <a href="http://maristas.org.br/" target="_blank" rel="noopener noreferrer">Província Marista Brasil Sul-Amazônia</a>
+                      </p>
+    </div>
                                                                                                            
     <footer>
     <p>© <?php echo date('Y'); ?> Marista Brasil - ABEC - CNPJ: 60.982.352/0053-42</p>
