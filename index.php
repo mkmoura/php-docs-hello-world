de2e9d148cf6d89751bd459691decf3d4d6dd290
MATEUS KOVALSKI <50926988+mkmoura@users.noreply.github.com>
2023-06-15 12:39:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<!DOCTYPE html> <html> <head> <title>Site em Manutenção</title> <style> body { background-color: #f1f1f1; font-family: Arial, sans-serif; text-align: center; padding-top: 100px; } h1 { color: #333333; } img { max-width: 400px; height: auto; } </style> </head> <body> <h1>Site em Manutenção</h1> <img src="imagem_manutencao.jpg" alt="Manutenção"> <p>Site em construção. Volte em breve!</p> </body> </html>
+<!DOCTYPE html> <html> 
+  <head> 
+    <title>
+      Site em Manutenção
+    </title> 
+    
+    <style> 
+      body 
+      { background-color: #f1f1f1; font-family: Arial, sans-serif; text-align: center; padding-top: 100px; } 
+      h1 { color: #333333; } 
+      img { max-width: 400px; height: auto; } 
+    </style> </head> 
+  <body>
+    <h1>
+      Site em Manutenção
+    </h1>
+    <img src="https://marista.edu.br/intranett/wp-content/uploads/2023/03/logo-mobile.png" alt="Manutenção"> 
+    
+    <p>
+      Site em construção. Volte em breve!
+    </p> 
+  </body> 
+</html>
