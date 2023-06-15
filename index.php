b20ddd9ad5f14d7299bef05044efd3f6ed016a42
MATEUS KOVALSKI <50926988+mkmoura@users.noreply.github.com>
2023-06-15 12:57:15 -0300

    Update imagens index.php

--- index.php
+++ index.php
@@ -6,19 +6,28 @@
     
     <style> 
       body 
-      { background-color: #f1f1f1; font-family: Arial, sans-serif; text-align: center; padding-top: 100px; } 
+      { 
+        background-color: #B2EBF2; /* Cor azul claro */ 
+        font-family: Arial, sans-serif; 
+        text-align: center; 
+        padding-top: 100px;
+      } 
       h1 { color: #333333; } 
       img { max-width: 400px; height: auto; } 
-    </style> </head> 
+    </style> 
+  </head> 
   <body>
     <h1>
       Site em construção
     </h1>
-    <img src="https://marista.edu.br/intranett/wp-content/uploads/2023/03/logo-mobile.png" alt="Manutenção"> 
+    <img src="https://marista.edu.br/intranett/wp-content/uploads/2023/03/logo-mobile.png" alt="Construção"> 
     
     <p>
       Site em construção. Volte em breve!
     </p> 
+    
+    <img src="https://umbrasil.org.br/wp-content/uploads/2010/01/MAPA-BRASIL-MARISTA-1.jpg alt="Construção">
+                                                                                                           
     <footer>
     <p>© <?php echo date('Y'); ?> Marista Brasil - ABEC - CNPJ: 60.982.352/0053-42</p>
     </footer>
